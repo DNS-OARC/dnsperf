a2e1732974116bbccbe8d73b9609a530e264a3cc
Jerry Lundström <lundstrom.jerry@gmail.com>
2019-01-17 14:30:52 +0100

    License
    - Update license headers

--- os.c
+++ os.c
@@ -1,11 +1,14 @@
 /*
- * Copyright (C) 2011-2016 Nominum, Inc.
+ * Copyright 2019 OARC, Inc.
+ * Copyright 2017-2018 Akamai Technologies
+ * Copyright 2006-2016 Nominum, Inc.
+ * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
