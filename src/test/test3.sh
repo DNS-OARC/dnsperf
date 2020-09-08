e3fb68592b3711e664cd7255c4a1dce6a2099a5c
Jerry Lundström <lundstrom.jerry@gmail.com>
2020-09-08 11:27:16 +0200

    Tests
    - Add more network tests
    - Add more invalid options tests

--- src/test/test3.sh
+++ src/test/test3.sh
@@ -2,3 +2,21 @@
 
 ! ../dnsperf -d does_not_exist
 ! ../resperf -d does_not_exist
+! ../dnsperf -f invalid
+! ../dnsperf -f any -s 256.256.256.256
+! ../dnsperf -f inet -s 256.256.256.256
+! ../dnsperf -f inet6 -s 256.256.256.256
+! ../dnsperf -a 127.0.0.1 -d does_not_exist
+! ../dnsperf -a ::1 -d does_not_exist
+! ../dnsperf -a 256.256.256.256
+! ../dnsperf -m invalid
+! ../dnsperf -n 43f8huishfs
+! ../dnsperf -p 12345 unexpected argument
+
+! ../resperf -d does_not_exist
+! ../resperf -r 0 -c 0
+! ../resperf -f invalid
+! ../resperf -q 256000
+! ../resperf -m 123.45 unexpected argument
+! ../resperf -m 123..
+! ../resperf -m 123a
